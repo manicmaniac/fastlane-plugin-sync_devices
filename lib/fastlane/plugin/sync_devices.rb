cbfc35c54255428159157fee002eeda06615d124
Ryosuke Ito <rito.0305@gmail.com>
2024-02-27 02:33:51 +0900

    Fastlane::SyncDevice.all_classes is necessary

--- lib/fastlane/plugin/sync_devices.rb
+++ lib/fastlane/plugin/sync_devices.rb
@@ -1,2 +1,14 @@
 require_relative 'sync_devices/version'
 require_relative 'sync_devices/actions/sync_devices_action'
+
+module Fastlane
+  module SyncDevices
+    def self.all_classes
+      Dir[File.expand_path('**/{actions,helper}/*.rb', __dir__)]
+    end
+  end
+end
+
+Fastlane::SyncDevices.all_classes.each do |current|
+  require current
+end
