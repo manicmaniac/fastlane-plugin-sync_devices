8471b516ed57abe1a2fbf7ceba3b8f740c17f7b5
Ryosuke Ito <rito.0305@gmail.com>
2025-01-26 01:46:53 +0900

    Separate script to start servers

--- spec/support/servers/proxy.rb
+++ spec/support/servers/proxy.rb
@@ -1,9 +1,6 @@
 # frozen_string_literal: true
 
 # MITM Proxy server that redirects SSL access of AppStore Connect API to our mock server.
-#
-# @example
-#   curl -x https://localhost:8888 -k https://api.appstoreconnect.apple.com/v1/devices
 
 require 'webrick'
 require 'webrick/httpproxy'
@@ -17,30 +14,17 @@ module WEBrick
 end
 
 class ProxyServer < WEBrick::HTTPProxyServer
+  attr_reader :app_port
+
+  def initialize(config)
+    @app_port = config.delete(:AppPort)
+    super
+  end
+
   def do_CONNECT(req, res) # rubocop:disable Naming/MethodName
     host = req.unparsed_uri.split(':', 2)[0]
-    req.unparsed_uri = 'localhost:4567' if host == 'api.appstoreconnect.apple.com'
+    req.unparsed_uri = "localhost:#{app_port}" if host == 'api.appstoreconnect.apple.com'
 
     super
   end
-end
-
-if (pid = fork)
-  proxy = ProxyServer.new({ Port: 8888 })
-  trap('INT') do
-    proxy.shutdown
-    Process.kill('INT', pid)
-  end
-  proxy.start
-else
-  app = Sinatra::Application
-  app.set(
-    :server_settings,
-    {
-      Port: 4567,
-      SSLEnable: true,
-      SSLCertName: [['CN', 'api.appstoreconnect.apple.com']]
-    }
-  )
-  app.start!
 end
