c234b3268d14250bd687087aa9686f9895466fef
Ryosuke Ito <rito.0305@gmail.com>
2025-01-26 02:30:02 +0900

    Manipulate instance variable directly instead of modify the class

--- server/proxy.rb
+++ server/proxy.rb
@@ -4,12 +4,6 @@ require 'webrick'
 require 'webrick/httpproxy'
 require 'webrick/https'
 
-module WEBrick
-  class HTTPRequest # rubocop:disable Style/Documentation
-    attr_writer :unparsed_uri
-  end
-end
-
 # MITM Proxy server that redirects SSL access of AppStore Connect API to our mock server.
 class ProxyServer < WEBrick::HTTPProxyServer
   attr_reader :app_port
@@ -21,8 +15,7 @@ class ProxyServer < WEBrick::HTTPProxyServer
 
   def do_CONNECT(req, res) # rubocop:disable Naming/MethodName
     host = req.unparsed_uri.split(':', 2)[0]
-    req.unparsed_uri = "localhost:#{app_port}" if host == 'api.appstoreconnect.apple.com'
-
+    req.instance_variable_set(:@unparsed_uri, "localhost:#{app_port}") if host == 'api.appstoreconnect.apple.com'
     super
   end
 end
