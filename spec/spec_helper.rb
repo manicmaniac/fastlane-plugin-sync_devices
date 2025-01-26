9aaa8affa7df606512bf41e7b52fac286fa5f698
Ryosuke Ito <rito.0305@gmail.com>
2025-01-26 13:29:19 +0900

    Output lcov for vscode-coverage-gutters

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,11 +3,21 @@
 $LOAD_PATH.unshift(File.expand_path('../lib', __dir__))
 
 require 'simplecov'
+require 'simplecov-lcov'
+
+# https://github.com/ryanluker/vscode-coverage-gutters/tree/v2.12.0/example/ruby#installation
+SimpleCov::Formatter::LcovFormatter.config do |c|
+  c.report_with_single_file = true
+  c.output_directory = 'coverage'
+  c.lcov_file_name = 'lcov.info'
+end
 
 SimpleCov.start do
   load_profile 'test_frameworks'
   enable_coverage :branch
   primary_coverage :branch
+  formatter SimpleCov::Formatter::MultiFormatter.new([SimpleCov::Formatter::LcovFormatter,
+                                                      SimpleCov::Formatter::HTMLFormatter])
 end
 
 # This module is only used to check the environment is currently a testing env
